5d39a9d2820c90d74a8a5f0d326ba707a5d79522
simloken <69955127+simloken@users.noreply.github.com>
2020-11-21 01:28:52 +0100

    Writing to data file finished
    Sample file included

--- Project_4/code/runner.py
+++ Project_4/code/runner.py
@@ -3,20 +3,32 @@ import multiprocessing as mp
 import numpy as np
 import itertools as it
 import time
+import sys
+import os
 
     
     
     
 if __name__ == '__main__':
-    threads = int(input('Please enter the number of threads you would like to use:\n'))
-    if threads > mp.cpu_count():
-        raise ValueError('Number of Threads cannot exceed available threads.\nMax threads: %i' %(mp.cpu_count))
-    
-    L = int(input('Please enter an integer value for L:\n'))
-    cycles = int(input('Please enter an integer value for number of Montecarlo Cycles:\n'))
-    T1 = float(input('Intial Temperature:\n'))
-    T2 = float(input('End Temperature:\n'))
-    TStep = float(input('Temperature Step:\n'))
+    if len(sys.argv) != 7:
+        print('Some initial inputs missing!\nRequired Inputs:[threads, L, cycles, T1, T2, TStep]\nPlease re-enter inputs below as requested')
+        threads = int(input('Please enter the number of threads you would like to use:\n'))
+        if threads > mp.cpu_count():
+            raise ValueError('Number of Threads cannot exceed available threads.\nMax threads: %i' %(mp.cpu_count))
+        L = int(input('Please enter an integer value for L:\n'))
+        cycles = int(input('Please enter an integer value for number of Montecarlo Cycles:\n'))
+        T1 = float(input('Intial Temperature:\n'))
+        T2 = float(input('Final Temperature:\n'))
+        TStep = float(input('Temperature Step Size:\n'))
+    else:
+        threads = int(sys.argv[1])
+        if threads > mp.cpu_count():
+            raise ValueError('Number of Threads cannot exceed available threads.\nMax threads: %i' %(mp.cpu_count))
+        L = int(sys.argv[2])
+        cycles = int(sys.argv[3])
+        T1 = float(sys.argv[4])
+        T2 = float(sys.argv[5])
+        TStep = float(sys.argv[6])
     excess = False; special = False; leftover = False
     dd = 0
     T = np.arange(T1, T2+TStep, TStep)
@@ -35,9 +47,9 @@ if __name__ == '__main__':
             
     with mp.Pool(processes=threads) as pool:
         t0 = time.perf_counter()
-        if excess != True:
+        if excess != True: #if there is not an excess in threads in relation to temperatures, assign a thread to each temperature
             results = [pool.apply_async(monteCarlo, (temp,L,cycles)) for temp in T]
-        elif excess == True:
+        elif excess == True: #if there is excess, assign additional threads to chunks/batches of cycles to speed up process
             if special == True:
                     results = ([pool.apply_async(monteCarlo, (temp,L,nCycles)) for temp in T for iter in range(d)])
             else:
@@ -47,9 +59,22 @@ if __name__ == '__main__':
                     for i in leftover_res:
                         results.append(i)
                 
-        preRes = np.array(([res.get() for res in results]))
-        sortRes = sorter(preRes, T, L, cycles)
+        preRes = np.array(([res.get() for res in results])) #extracts all values
+        t1 = time.perf_counter() - t0
+        sortRes = sorter(preRes, T, L, cycles) #sorts and scales/normalizes the values
         print(sortRes)
-        print('\nTime:\n%f' %(time.perf_counter() - t0))
+        print('\nTime:\n%f' %(t1))
         print('\nComplete!')
+        cwd = os.getcwd()
+        os.chdir("../data/")
+        if os.path.isfile('data.txt') == False: #creates files and labels for first run
+            file = open('data.txt', 'w+')
+            file.write('Cycles\tNSpins\tTemp\tMeanE\t\tEnergyVar\tMeanM\t\tMVar\t\tMeanAbsM\tTime\n')
+            file.close()
+        file = open('data.txt', 'a')
+        for i in sortRes:
+            file.write("%i\t%i\t%0.3f\t%f\t%f\t%f\t%f\t%f\t%g\n" %(cycles,L,i[0],i[1],i[2],i[3],
+                                                                i[4], i[5], t1))
+        file.close()
+        os.chdir(cwd)
         last = (input('Press Enter to close program'))
